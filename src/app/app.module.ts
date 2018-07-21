66625d82ad2d67afae904fc596730829c128bfd5
Maina Wycliffe <wmmaina7@gmail.com>
2018-07-21 10:39:23 +0300

    integrated with the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestLibraryModule, TestLibConfig } from 'test-library';
+
+const testLibConfigs: TestLibConfig = {
+  title: 'World Cup Teams',
+  teams: [
+    {
+      country: 'France',
+      trophies: 2
+    },
+    {
+      country: 'Brazil',
+      trophies: 5
+    },
+    {
+      country: 'Germany',
+      trophies: 4
+    },
+    {
+      country: 'Argentina',
+      trophies: 2
+    },
+
+    {
+      country: 'Spain',
+      trophies: 1
+    },
+    {
+      country: 'England',
+      trophies: 1
+    },
+    {
+      country: 'Uruguay',
+      trophies: 2
+    },
+    {
+      country: 'Italy',
+      trophies: 4
+    }
+  ],
+  sport: 'Football/Soccer'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TestLibraryModule,
+    TestLibraryModule.forRoot(testLibConfigs)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
